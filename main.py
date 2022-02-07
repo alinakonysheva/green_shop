0f0d9877edfa95ccdc72cd6ebbfa5d942c9359d4
Alina Konysheva <konysheva.alina@gmail.com>
2022-02-07 19:02:29 +0100

    added text of the task

---
+++ main.py
@@ -0,0 +1,11 @@
+# Een “webshop” maken. In onze webshop verkopen we boeken, zowel fysieke als audio als voor een e reader.
+# Elk boek heeft uiteraard een paar eigenschappen. Elke klant heeft uiteraard ook een paar eigenschappen
+# Wat moet er gemaakt worden:
+# - een klantenbestand
+#  - product pagina(’s)
+#  - gemakkelijke filters op product
+#  - login/wachtwoord reset
+#  - een wenslijst van een klant, een klant kan dus een boek op zijn wenslijst zetten en uiteraard daar
+# ook afhalen
+# Note: het is niet de bedoeling dat we een betaal en/of facturatie systeem en dergelijke gaan toevoegen.
+# Ook geen shopping cart
