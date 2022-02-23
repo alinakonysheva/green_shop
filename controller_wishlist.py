ce2a9cd26fe168d0952117d9fc4781e5f0429082
Alina Konysheva <konysheva.alina@gmail.com>
2022-02-23 17:22:10 +0100

    unittests of wishlist controller, except for not happy paths

--- controller_wishlist.py
+++ controller_wishlist.py
@@ -1,69 +1,83 @@
 from wishlist import Wishlist
-from database import session
 
 
 class ControllerWishlist:
-    @staticmethod
-    def add_wishlist(id_user):
+    def __init__(self, session):
+        self.session = session
+
+    def add_wishlist(self, id_user):
+        # TODO: IF id_user exists, else raise exception
         wl = Wishlist()
         wl.id_user = id_user
 
-        session.add(wl)
-        session.commit()
+        self.session.add(wl)
+        self.session.commit()
+        return wl.id
 
-    @staticmethod
-    def change_user_in_wishlist(id_wishlist, id_user) -> None:
+    def change_user_in_wishlist(self, id_wishlist, id_user) -> None:
         """
         to update user's id in the wishlist
         :param id_wishlist
         :param id_user
         """
-        wl = session.query(Wishlist).get(id_wishlist)
-        wl.id_user = id_user
+        try:
+            wl = self.session.query(Wishlist).get(id_wishlist)
+            if wl:
+                wl.id_user = id_user
+                self.session.add(wl)
+                self.session.commit()
+            else:
+                raise ValueError(f'Wishlist with #{id_wishlist} does nor exist in database')
+        except Exception as e:
+            print(e)
 
-        session.add(wl)
-        session.commit()
+    def remove_wishlist(self, id_wishlist):
+        try:
+            wl = self.session.query(Wishlist).get(id_wishlist)
+            if wl:
+                self.session.delete(wl)
+                self.session.commit()
+            else:
+                raise ValueError(f'Wishlist with #{id_wishlist} does not exist in database')
+        except Exception as e:
+            print(e)
 
-    @staticmethod
-    def remove_wishlist(id_wishlist):
-        wl = session.query(Wishlist).get(id_wishlist)
-        session.delete(wl)
-        session.commit()
-
-    @staticmethod
-    def get_wishlist(id_wishlist):
+    def get_wishlist_by_id(self, id_wishlist):
         """
         to get wishlist by wishlist's id
         :param id_wishlist:
         :return:
         """
-        wl = session.query(Wishlist).get(id_wishlist)
-        return wl
+        wl = self.session.query(Wishlist).get(id_wishlist)
+        if wl:
+            return wl
+        else:
+            raise ValueError(f'Wishlist with #{id_wishlist} does not exist in database')
 
-    @staticmethod
-    def get_wishlist_by_user(id_user):
+    def get_wishlist_by_user(self, id_user):
         """
         to get a wishlist by user id
         :param id_user
         :return: instance of  Wishlist by id_user
         """
-        wl = session.query(Wishlist).filter(Wishlist.id_user == id_user).all()[0]
+        # TODO: if id_user exists!!!
+
+        wl = self.session.query(Wishlist).filter(Wishlist.id_user == id_user).all()[0]
         return wl
 
-    @staticmethod
-    def is_wishlist_by_user(id_user) -> object:
-        wl = session.query(Wishlist).filter(Wishlist.id_user == id_user).all()[0]
+    def is_wishlist_by_user(self, id_user) -> object:
+        # TODO: if id_user exists!!!
+        wl = self.session.query(Wishlist).filter(Wishlist.id_user == id_user).all()[0]
         if wl:
             return True
         else:
             return False
 
     # get all existing ids
-    @staticmethod
-    def get_all_ids():
+    def get_all_wishlists_ids(self):
         """
         to get list of all existing id's
         :return: list of str(id)
         """
-        id_list = list(map(lambda x: x.id, session.query(Wishlist).all()))
+        id_list = list(map(lambda x: x.id, self.session.query(Wishlist).all()))
         return id_list
--- test_wishlist.py
+++ test_wishlist.py
@@ -5,6 +5,7 @@ from database import create_database
 from sqlalchemy.orm import sessionmaker
 from wishlist import Wishlist
 from user import User
+from controller_wishlist import ControllerWishlist
 
 
 class BaseDbTest(TestCase):
@@ -34,3 +35,61 @@ class WishlistTests(BaseDbTest):
         wl_from_query = self.session.query(Wishlist).get(1)
         self.assertEqual(wl_from_query.id_user, wishlist.id_user)
 
+
+class ControllerWishlistTests(BaseDbTest):
+    def do_setup(self):
+        wishlist = Wishlist()
+        user_1 = User()
+        wishlist.id_user = user_1.id
+
+        self.controller = ControllerWishlist(self.session)
+        self.session.add(wishlist)
+        self.session.commit()
+        self.wishlist = wishlist
+        self.user = user_1
+
+    def test_get_wishlist(self):
+        id_wishlist = self.wishlist.id
+        wl = self.controller.get_wishlist_by_id(id_wishlist)
+        self.assertEqual(wl.id_user, self.user.id)
+
+    def test_change_user_in_wishlist(self):
+        user_2 = User()
+        id_user_2 = user_2.id
+        self.controller.change_user_in_wishlist(self.wishlist.id, id_user_2)
+        wl = self.controller.get_wishlist_by_id(self.wishlist.id)
+        self.assertEqual(id_user_2, wl.id_user)
+
+    def test_add_wishlist(self):
+        user_3 = User()
+        id_user_3 = user_3.id
+        id_new_wishlist = self.controller.add_wishlist(id_user_3)
+        wl = self.controller.get_wishlist_by_id(id_new_wishlist)
+        self.assertEqual(wl.id_user, id_user_3)
+
+    def test_remove_wishlist(self):
+        # TODO: with real users
+        # user_3 = User()
+        # id_user_3 = user_3.id
+        id_user_3 = 3
+        id_new_wishlist = self.controller.add_wishlist(id_user_3)
+        self.controller.remove_wishlist(id_new_wishlist)
+        with self.assertRaises(ValueError):
+            self.controller.get_wishlist_by_id(id_new_wishlist)
+
+    def test_get_wishlist_by_user(self):
+        wl = self.controller.get_wishlist_by_user(self.user.id)
+        self.assertEqual(wl.id, self.wishlist.id)
+
+    def test_is_wishlist_by_user(self):
+        self.assertTrue(self.controller.get_wishlist_by_user(self.user.id))
+
+    def test_get_all_wishlists_ids(self):
+        list_with_wl_ids = self.controller.get_all_wishlists_ids()
+        self.assertTrue(type(list_with_wl_ids) == list)
+        self.assertIn(self.wishlist.id, list_with_wl_ids)
+
+        user5 = User()
+        wl_id = self.controller.add_wishlist(user5.id)
+        list_with_wl_ids_2 = self.controller.get_all_wishlists_ids()
+        self.assertIn(wl_id, list_with_wl_ids_2)
