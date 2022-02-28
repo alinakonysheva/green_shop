1cc30cab238704e1717713bbacd9a675105db338
Alina Konysheva <konysheva.alina@gmail.com>
2022-02-28 11:36:30 +0100

    address, user: v - > char, erased id from address en user (they have ids from BaseObj), _user in Address - > user, @street.setter -> @city.setter, postcode.setter -> @postcode.setter

--- address.py
+++ address.py
@@ -6,63 +6,63 @@ from sqlalchemy.orm import relationship
 
 class Address(BaseObj):
     __tablename__ = "T_ADDRESS"
-    _Id = Column('ADDRESS_ID',Integer,primary_key=True)
-    _street = Column('STREET',String(88))
-    _number = Column('NUMBER',String(10))
+    _street = Column('STREET', String(88))
+    _number = Column('NUMBER', String(10))
     _city = Column('CITY', String(200))
-    _country = Column('C0UNTRY',String(30))
-    _postcode =Column('POSTCODE', Integer)
-    _user = relationship('User',back_populates="address")
+    _country = Column('C0UNTRY', String(30))
+    _postcode = Column('POSTCODE', Integer)
+    user = relationship('User', back_populates="address")
 
     @hybrid_property
     def street(self):
         return self._street.capitalize()
+
     @street.setter
-    def street(self,value):
+    def street(self, value):
         self._street = value
 
     @hybrid_property
     def number(self):
         return self._number.capitalize()
+
     @number.setter
     def number(self, value):
         v = value.strip()
         special_characters = '!@ # $%^&*()-+?_=,<">/'
         for char in v:
-            if v in special_characters:
-                raise valueError('a number cannot contain special characters')
+            if char in special_characters:
+                raise ValueError('a number cannot contain special characters')
         self._number = value
 
     @hybrid_property
     def city(self):
         return self._city.capitalize()
-    @street.setter
+
+    @city.setter
     def city(self, value):
         self._city = value
 
     @hybrid_property
     def country(self):
         return self._country.capitalize()
+
     @country.setter
     def country(self, value):
         v = value.strip()
         special_characters = '!@ # $%^&*()-+?_=,<">/'
         for char in v:
-            if v in special_characters:
-                raise valueError('a country cannot contain special characters')
+            if char in special_characters:
+                raise ValueError('a country cannot contain special characters')
         self._country = value
+
     @hybrid_property
     def postcode(self):
         return self._postcode
-    postcode.setter
-    def postcode(self,value):
+
+    @postcode.setter
+    def postcode(self, value):
         v = value.strip()
         for i in v:
             if i is not int:
-                raise valueError('a postcode only contains numbers')
+                raise ValueError('a postcode only contains numbers')
         self._postcode = value
-
-
-
-
-
--- user.py
+++ user.py
@@ -7,7 +7,6 @@ from address import Address
 
 class User(BaseObj):
     __tablename__ = "T_USER"
-    id = Column('USER_ID', Integer, primary_key=True)
     _firstname = Column('FIRST_NAME', String())
     _lastname = Column('LAST_NAME', String())
     _email = Column('EMAIL', String())
@@ -16,7 +15,6 @@ class User(BaseObj):
     wishlist = Column('WISHLIST', Integer)
     address_id = Column('ADDRESS_ID', ForeignKey(Address.addressid), index=True)
     address = relationship(Address, Foreign_keys='User.addresid', back_populates="user")
-
 
     @hybrid_property
     def firstname(self):
@@ -27,7 +25,7 @@ class User(BaseObj):
         v = value.strip()
         special_characters = '!@ # $%^&*()-+?_=,<">/'
         for char in v:
-            if v in special_characters:
+            if char in special_characters:
                 raise ValueError('a name cannot contain special characters')
         self._firstname = value
 
@@ -53,9 +51,9 @@ class User(BaseObj):
         v = value.strip()
         valid = False
         for char in v:
-            if v == '@':
+            if char == '@':
                 valid = True
-        if valid == True:
+        if valid:
             self._email = value
         else:
             raise ValueError('no valid email')
@@ -67,7 +65,7 @@ class User(BaseObj):
     @status.setter
     def status(self, value):
         if value == 0 or value == 1:
-            self._status = value
+            self.status = value
         else:
             raise ValueError('no valid admin status')
 
