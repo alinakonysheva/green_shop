892d03a2cf6403ea18f3fbc5958191e6bead80a1
Alina Konysheva <konysheva.alina@gmail.com>
2022-03-01 13:25:28 +0100

    done: unittest for user with relations

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from database import create_database
 from sqlalchemy.orm import sessionmaker
 from user import User
+from wishlist import Wishlist
+from address import Address
 
 C_LAST_NAME = 'Yalom'
 C_LAST_NAME_2 = 'Yalom2'
@@ -15,8 +17,31 @@ C_INCORRECT_FIRST_NAME_LONG = str('[i for i in ...]')
 C_INCORRECT_FIRST_NAME_SHORT = ''
 C_EMAIL = 'irvinYalom@gmail.com'
 C_STATUS = 1
-#TODO: add reletionship with wishlist
-#TODO: check relationship with address
+
+C_TITLE = 'Staring at the Sun'
+
+C_STREET = 'Baker Street'
+C_NUMBER = '221B'
+C_CITY = 'London'
+C_COUNTRY = 'Great Britain'
+C_POSTCODE = 'NW1 6XE'
+
+C_TITLE = 'Staring at the Sun'
+C_AUTHOR_LAST_NAME = 'Yalom'
+C_AUTHOR_FIRST_NAME = 'Irvin'
+C_AUTHOR_MIDDLE_NAME = 'David'
+C_RELEASE_YEAR = 2008
+C_RATING = 9.9
+C_PIC = '/dsfgasdfgad'
+C_CATEGORY = 3
+C_LANGUAGE = "nl"
+C_ANNOTATION = "Written in Irv Yalom's inimitable story-telling style, Staring at the Sun is a profoundly encouraging" \
+               "approach to the universal issue of mortality. In this magisterial opus, capping a lifetime of work and" \
+               "personal experience, Dr. Yalom helps us recognize that the fear of death is at the heart of much of " \
+               "our anxiety. Such recognition is often catalyzed by an \"awakening experience\"—a dream, \" \
+               \"or loss (the death of a loved one, divorce, loss of a job or home), illness, trauma, or aging."
+C_PUBLISHER = "Wiley"
+C_SIZE = 56.0
 
 
 class BaseDbTest(TestCase):
@@ -43,6 +68,21 @@ class UserTests(BaseDbTest):
         user.email = C_EMAIL
         user.status = C_STATUS
 
+        address = Address()
+        address.street = C_STREET
+        address.number = C_NUMBER
+        address.city = C_CITY
+        address.country = C_COUNTRY
+        address.postcode = C_POSTCODE
+        user.address = [address]
+        self.session.add(address)
+        self.session.commit()
+        wishlist = Wishlist()
+        wishlist.id_user = user.id
+        self.session.add(wishlist)
+        self.session.commit()
+
+        user.wishlist = [wishlist]
         self.session.add(user)
         self.session.commit()
 
@@ -51,3 +91,11 @@ class UserTests(BaseDbTest):
         self.assertEqual(user_from_query.lastname, C_LAST_NAME)
         self.assertEqual(user_from_query.email, C_EMAIL)
         self.assertEqual(user_from_query.status, C_STATUS)
+        wishlist_from_user_from_query = user_from_query.wishlist[0]
+        self.assertEqual(wishlist_from_user_from_query.id_user, user.id)
+        address_from_user_from_query = user_from_query.address[0]
+        self.assertEqual(address_from_user_from_query.street, C_STREET.capitalize())
+        self.assertEqual(address_from_user_from_query.number, C_NUMBER.capitalize())
+        self.assertEqual(address_from_user_from_query.city, C_CITY.capitalize())
+        self.assertEqual(address_from_user_from_query.country, C_COUNTRY.capitalize())
+        self.assertEqual(address_from_user_from_query.postcode, C_POSTCODE)
